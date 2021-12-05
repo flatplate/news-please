5cc1e72d0dd8c204e9d2e854bb8336bc9b16f776
Ural Bayhan <uralbayhan0@gmail.com>
2021-12-05 20:58:25 +0100

    Add kafka sink pipeline element
    This commit implements a pipeline element that
    can be used to push the articles into a kafka
    topic.

--- newsplease/pipeline/pipelines.py
+++ newsplease/pipeline/pipelines.py
@@ -13,6 +13,7 @@ import psycopg2
 from dateutil import parser as dateparser
 from elasticsearch import Elasticsearch
 from scrapy.exceptions import DropItem
+from kafka import KafkaProducer
 
 from NewsArticle import NewsArticle
 from .extractor import article_extractor
@@ -656,6 +657,27 @@ class ElasticsearchStorage(ExtractedInformationStorage):
                 self.running = False
                 self.log.error("Lost connection to Elasticsearch, this module will be deactivated: %s" % error)
         return item
+
+class KafkaProducerSink(ExtractedInformationStorage):
+
+    def __init__(self):
+        super().__init__()
+        self.kafka_config = self.cfg.section("Kafka")
+        self.bootstrap_servers = self.kafka_config.get("bootstrap_servers")
+        self.topic = self.kafka_config.get("topic", "newsplease")
+        self.kafka_version = tuple(self.kafka_config.get("api_version", [3, 0, 0]))
+        self.producer = KafkaProducer(bootstrap_servers=self.bootstrap_servers,
+                                      value_serializer=self.default_json_serializer,
+                                      api_version=self.kafka_version)
+
+    def process_item(self, item, spider):
+        item_dict = self.extract_relevant_info(item)
+        self.producer.send(self.topic, value=item_dict)
+        return item
+
+    @staticmethod
+    def default_json_serializer(value):
+        return json.dumps(value).encode('utf-8')
 
 
 class DateFilter(object):
